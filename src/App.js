856eae55f5b0b1192709e406e9f84c2be094a257
l0o0gy <minamahone9620@gmail.com>
2024-10-01 01:33:51 +0300

    add path to coffedetails page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import NavBar from './Components/NavBar'
+import CoffeDetails from './Pages/CoffeDetails';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <NavBar/>
         <Routes>
           <Route path='/' element={<HomePage />} />
+          <Route path='/miswagcoffee/:dataId/:dataName' element={<CoffeDetails/>}/>
         </Routes>
       </BrowserRouter>
     </div>
