4f87e4d04bef20d72ac7d7991bd8f2c5daf44dc7
l0o0gy <minamahone9620@gmail.com>
2024-09-30 17:22:30 +0300

    add route to home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-lime-500'> hello world </h1>
+    <div className="font-sans ">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
