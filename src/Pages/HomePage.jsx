21d9ed536020904aaa8e05091013e69bc0d69a27
l0o0gy <minamahone9620@gmail.com>
2024-09-30 21:52:29 +0300

    add card component

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import NavBar from '../Components/NavBar'
 import HeroSection from '../Components/HeroSection'
+import Card from '../Components/Card'
+
 
 function HomePage() {
+
   return (
     <div>
         <NavBar/>
         <HeroSection/>
+        <Card/>
     </div>
   )
 }
