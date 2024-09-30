91af1b8b5b2d8946ec27ba6c48cd4d9f5d84f650
l0o0gy <minamahone9620@gmail.com>
2024-09-30 19:08:41 +0300

    add herosection

---
+++ src/Components/HeroSection.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function HeroSection() {
+    const [miswagData, setMiswagData] = useState([]); 
+    const [error, setError] = useState(null); 
+
+    useEffect(() => {
+        const fetchCoffeeShopData = async () => {
+            try {
+                const response = await axios.get('https://file.notion.so/f/f/f9a09310-af94-4993-bbca-d051d7b65e1d/2dd59431-7382-4f93-9625-fece6ad64e7d/coffee.json');
+                setMiswagData(response.data);
+                console.log(response.data); 
+            } catch (err) {
+                setError(err); 
+                console.error(err);
+            }
+        };
+
+        fetchCoffeeShopData();
+    }, []);
+
+    return (
+        <div>
+            <div className='bg-slate-400 rounded-xl h-72 ml-20 mr-20 flex overflow-x-auto'> 
+                {miswagData.length > 0 && miswagData.map((data) => (
+                    <img 
+                        key={data.id}
+                        src={data.image} 
+                        alt={data.name}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default HeroSection;
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from '../Components/NavBar'
+import HeroSection from '../Components/HeroSection'
 
 function HomePage() {
   return (
     <div>
         <NavBar/>
+        <HeroSection/>
     </div>
   )
 }
