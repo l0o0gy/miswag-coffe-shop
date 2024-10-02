2c871c47446088f1722ab90d1369f647b3e035c6
l0o0gy <minamahone9620@gmail.com>
2024-10-02 04:28:17 +0300

    add path to favorit and cover

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
-import NavBar from './Components/NavBar'
+import NavBar from './Components/NavBar';
 import CoffeDetails from './Pages/CoffeDetails';
-
+import FavoritesCoffee from './Pages/FavoritesCoffee';
+import Cover from './Components/Cover';
 
 function App() {
   return (
-    <div className="font-sans ">
+    <div className="font-sans">
       <BrowserRouter>
-      <NavBar/>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/miswagcoffee/:dataId/:dataName' element={<CoffeDetails/>}/>
-        </Routes>
+        <MainContent />
       </BrowserRouter>
     </div>
+  );
+}
+
+function MainContent() {
+  const location = useLocation();
+
+  const showNavBar = location.pathname !== '/';
+
+  return (
+    <>
+      {showNavBar && <NavBar />}
+      <Routes>
+        <Route path='/' element={<Cover />} />
+        <Route path='/home' element={<HomePage />} />
+        <Route path='/miswagcoffee/:dataId/:dataName' element={<CoffeDetails />} />
+        <Route path='/favorites' element={<FavoritesCoffee />} />
+      </Routes>
+    </>
   );
 }
 
