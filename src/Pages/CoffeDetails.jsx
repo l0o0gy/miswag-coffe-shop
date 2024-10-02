8f64d9e3917f90d3ff3e4715c1eabd783fd1d990
l0o0gy <minamahone9620@gmail.com>
2024-10-02 10:14:45 +0300

    add selectedcup rouls

--- src/Pages/CoffeDetails.jsx
+++ src/Pages/CoffeDetails.jsx
@@ -6,11 +6,12 @@ import fullcup from '../assets/img/coffee.png';
 function CoffeeDetails() {
     const location = useLocation();
     const { data } = location.state || {};
-    const [fullCups, setFullCups] = useState({ s: false, m: false, l: false });
+    const [selectedCup, setSelectedCup] = useState(null);
     const [coffeeCount, setCoffeeCount] = useState(1);
 
     const handleCupClick = (size) => {
-        setFullCups((prev) => ({ ...prev, [size]: !prev[size] }));
+        setSelectedCup(size);
+        setCoffeeCount(1);
     };
 
     const getCupWidth = (size) => {
@@ -46,13 +47,15 @@ function CoffeeDetails() {
             />
             <div className="md:pl-10 pt-5 md:pt-0">
                 <h1 className='font-bold text-2xl md:text-5xl text-amber-900'>{data.name}</h1>
-                <p className='text-sm md:text-xl md:mt-2 line-clamp-3 text-amber-700 pt-1 md:pt-10'>{data.description}</p>
+                <p className='text-sm md:text-xl  line-clamp-3 text-amber-700 text-opacity-80 pt-1 md:pt-5'> Description :</p>
+                <p className='text-sm md:text-xl line-clamp-3 text-amber-700 text-opacity-50 pt-1 md:pt-1'>{data.description}</p>
+                <p className='text-sm md:text-xl  line-clamp-3 text-current pt-1 text-opacity-50 md:pt-3'>Choose your Fav cup size : </p>
 
-                <div className="flex md:justify-center items-center mt-4 md:mt-6 space-x-4">
+                <div className="flex md:justify-center items-center mt-2 md:mt-2 space-x-4">
                     {['s', 'm', 'l'].map((size) => (
                         <div key={size} className="flex flex-col items-center">
                             <img
-                                src={fullCups[size] ? fullcup : emptycup}
+                                src={selectedCup === size ? fullcup : emptycup}
                                 alt={`Cup ${size}`}
                                 onClick={() => handleCupClick(size)}
                                 className={`cursor-pointer ${getCupWidth(size)}`}
@@ -61,32 +64,41 @@ function CoffeeDetails() {
                         </div>
                     ))}
                 </div>
-                <div className="flex items-center mt-4">
-                    <button
-                        onClick={decrementCount}
-                        className="bg-orange-500 hover:bg-orange-500 rounded-md   w-7 h-7  text-2xl flex justify-center items-center "
-                    >
-                        -
-                    </button>
-                    <span className="mx-4 text-lg">{coffeeCount}</span>
-                    <button
-                        onClick={incrementCount}
-                        className="bg-orange-500 hover:bg-orange-500 rounded-md  w-7 h-7  text-xl"
-                    >
-                        +
-                    </button>
-                </div>
 
-                <div className='relative h-12 w-40 md:h-12'>
-                    <p className='font-bold text-xl md:text-3xl text-orange-600 absolute bottom-0 left-0 h-5 w-2'>
-                        ${data.price}
-                    </p>
-                </div>
-                <div className='relative h-3 w-full'>
-                    <button className='bg-orange-500 hover:bg-orange-600 absolute bottom-0 right-0 rounded-lg p-2 text-white shadow-md'>
-                        Online Order
-                    </button>
-                </div>
+                {selectedCup && (
+                    <>
+                        <div className="flex items-center mt-1">
+                            <button
+                                onClick={decrementCount}
+                                className="bg-orange-500 hover:bg-orange-500 rounded-md   w-7 h-7  text-2xl flex justify-center items-center "
+                            >
+                                -
+                            </button>
+                            <span className="mx-4 text-lg">{coffeeCount}</span>
+                            <button
+                                onClick={incrementCount}
+                                className="bg-orange-500 hover:bg-orange-500 rounded-md  w-7 h-7  text-xl"
+                            >
+                                +
+                            </button>
+                        </div>
+
+                        <div className='relative h-12 w-40 md:h-9 mt-3'>
+                            <p className='font-bold text-xl md:text-3xl text-orange-600'>
+                                {(data.price * coffeeCount)} IQD
+                            </p>
+                        </div>
+
+
+
+                        <div className='relative  w-full'>
+                            <button className='bg-orange-500 hover:bg-orange-600 absolute bottom-0 right-0 rounded-lg p-2 text-white shadow-md'>
+                                Order Online
+                            </button>
+                        </div>
+                    </>
+                )}
+
             </div>
         </div>
     );
