632b4064f8a39cd11a687972ac3f87751f50a8ec
l0o0gy <minamahone9620@gmail.com>
2024-10-02 04:30:03 +0300

    add path

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,40 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/img/Picsart_24-09-30_16-38-04-777.png';
-import { useState } from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Drawer } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
-    const navItems = ['Store', 'Favorite', 'Blog', 'About'];
+    const navItems = [
+        { title: 'Store',path:'/home'},
+        { title: 'Favorite', path: '/favorites' },
+        { title: 'Blog' },
+        { title: 'About' } 
+    ];
+
     const [isOpen, setIsOpen] = useState(false);
-    const onpenDrawer = (open) => () => {
+    const openDrawer = (open) => () => {
         setIsOpen(open);
-    }
+    };
 
     return (
-        <nav className="flex items-center md:justify-between p-2  ">
+        <nav className="flex items-center md:justify-between p-2">
             <div className='md:hidden'>
-                <div onClick={onpenDrawer(true)} className='hover:bg-slate-100 hover:rounded-full p-2 '>
+                <div onClick={openDrawer(true)} className='hover:bg-slate-100 hover:rounded-full p-2'>
                     <MenuIcon />
                 </div>
             </div>
-            <img src={logo} alt="Miswag Logo" className="w-28 pt-1 md:pt-0 md:w-40  md:ml-16" />
+            <img src={logo} alt="Miswag Logo" className="w-28 pt-1 md:pt-0 md:w-40 md:ml-16" />
 
             <div className="hidden md:flex space-x-7 mr-20">
                 {navItems.map(item => (
-                    <h1 key={item} className="text-amber-900 hover:text-orange-500 font-semibold">
-                        {item}
-                    </h1>
+                    <Link to={item.path} key={item.title} style={{ textDecoration: 'none' }}>
+                        <h1 className="text-amber-900 hover:text-orange-500 font-semibold">
+                            {item.title}
+                        </h1>
+                    </Link>
                 ))}
             </div>
-            <Drawer anchor="left" open={isOpen} onClose={onpenDrawer(false)}>
-                <div className="flex flex-col p-4 w">
-                <img src={logo} alt="Miswag Logo" className="w-28 pt-1 " />
+
+            <Drawer anchor="left" open={isOpen} onClose={openDrawer(false)}>
+                <div className="flex flex-col p-4" style={{ width: '250px' }}>
+                    <img src={logo} alt="Miswag Logo" className="w-28 pt-1" />
 
                     {navItems.map(item => (
-                        <h1 key={item} className="text-amber-900 hover:text-orange-500 font-semibold mb-4 ml-3 mt-5 ">
-                            {item}
-                        </h1>
+                        <Link to={item.path} key={item.title} style={{ textDecoration: 'none' }}>
+                            <h1 className="text-amber-900 hover:text-orange-500 font-semibold mb-4 ml-3 mt-5">
+                                {item.title}
+                            </h1>
+                        </Link>
                     ))}
                 </div>
             </Drawer>
